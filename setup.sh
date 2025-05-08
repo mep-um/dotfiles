fbef5549c77ba71599ad5504891444a084ede499
mep-um <3371600+mep-um@users.noreply.github.com>
2025-05-08 08:01:35 -0400

    refactor(setup): Overhaul Zsh, Git configuration and setup script
    - .gitconfig: Streamline by removing commit template integration, a
      complex alias, and local include path to simplify global Git
      configuration.
    - .gitmessage: Delete as the commit template is no longer referenced
      in .gitconfig.
    - p10k.zsh: Delete as Powerlevel10k configuration is now handled
      within setup.sh and default Zsh settings.
    - setup.sh: Expand to automate more of the environment setup,
      including iTerm2, Powerlevel10k theme and fonts, common Zsh
      plugins (autocomplete, autosuggestions, syntax-highlighting), and
      refine rcm usage. Homebrew and Oh My Zsh installation methods
      updated.
    - .zshrc: Delete as its previous responsibilities for Zsh
      customization, NVM, and virtualenv are now either integrated into
      setup.sh, handled by Oh My Zsh, or deemed unnecessary for the base
      dotfiles.
    
    BREAKING CHANGE: The setup.sh script has been significantly
    overhauled. Previous .zshrc and p10k.zsh configurations are
    removed; the new setup.sh script attempts to recreate a similar
    environment. The .gitconfig no longer includes ~/.gitconfig.local,
    so any local Git customizations must be re-applied or moved.
    
    #release: major

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ brew upgrade
 brew doctor
 
 # Iterm2
-brew cask install iterm2
+brew install --cask iterm2
 
 # Curl
 echo "Installing curl..."
@@ -24,9 +24,9 @@ brew install wget
 # Python
 echo "Installing Python environment..."
 brew install python
-brew install pip
 
 # Development tools
+echo "Installing development tools..."
 brew install sqlite
 
 # git
@@ -34,27 +34,39 @@ echo "Installing git..."
 brew install git
 
 # zsh & oh-my-zsh
-echo "Installing zsh & oh-my-zsh..."
+echo "Installing terminal environment..."
 brew install zsh
-brew install zsh-autocomplete
-brew install zsh-autosuggestions
-brew install zsh-syntax-highlighting
 chsh -s /opt/homebrew/bin/zsh
-brew install powerlevel10k
-echo "source $(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-brew tap homebrew/cask-fonts
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+# powerlevel10k
+# Powerlevel10k is installed via git clone for Oh My Zsh (see line 45)
+# Set ZSH_THEME for Oh My Zsh to use Powerlevel10k
+echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# Set theme
+sed -i'.bak' 's|^ZSH_THEME=.*|ZSH_THEME="powerlevel10k/powerlevel10k"|' ~/.zshrc
+# Install fonts
+brew install alfred
 brew install font-hack-nerd-font
 # Manual Action: Set iterm2 color profile to "Solarized Dark"
 
-# dotfiles
-#echo "Installing dotfiles..."
-#git clone git@github.com:mep-um/dotfiles.git
+# zsh plugins
+brew install zsh-autocomplete
+echo "source $(brew --prefix)/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh" >> ~/.zshrc
+brew install zsh-autosuggestions
+echo "source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+brew install zsh-syntax-highlighting
+echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
 
 # rcm
 brew install rcm
 env RCRC=$HOME/dotfiles/rcrc rcup
-rcup -v
+rcup -v -x License -x readme.md -x setup.sh
 
-# Restart iTerm2
+
+source ~/.zshrc
+
+# set iterm theme
+echo "MANUAL STEPS:"
+echo "1. iTerm: Set Solarized Dark color theme for default profile."
